a9b54ecca247a00b1022189a069fe7a9697600b0
Ritvik Dayal <ritvikr1605@gmail.com>
2021-04-11 22:28:50 +0530

    added comments

--- users/models.py
+++ users/models.py
@@ -1,40 +1,66 @@
-from django.db import models
+# Django utility imports
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+# Django default model extentions
+from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-class CustomUserManager(BaseUserManager):
 
+class CustomUserManager(BaseUserManager):
+    '''Custom user manager
+    A custom account manager to handle user and super user account creations.
+    
+    Functions: 
+        - create_user : to handle creation of a default user account
+        - create_superuser : to handle creation of super_user account
+    '''
     def create_user(self, email, user_name, first_name, password, **other_fields):
-        
+        '''
+        This manager creates a simple user account for the given email.
+        '''
         if not email:
             raise ValueError(_('You must provide an email address.'))
 
+        # cleaning mail address.
         email = self.normalize_email(email)
+
+        # creating user model object        
         user = self.model(email=email, user_name=user_name, first_name=first_name, password=password, **other_fields)
         
+        # setting password for the user
         user.set_password(password)
         
+        # saving user model instance
         user.save()
         
         return user
 
     def create_superuser(self, email, user_name, first_name, password, **other_fields):
+        '''
+        This manager creates a super user account for the given email.
+        '''
 
+        # setting superuser, staff, active status as true by default
         other_fields.setdefault('is_superuser', True)
         other_fields.setdefault('is_staff', True)
         other_fields.setdefault('is_active', True)
 
+        # checking for staff status
         if other_fields.get('is_staff') is not True:
             raise ValueError('Superuser must be assigned to is_staff=True.')
 
+        #checking for superuser status
         if other_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must be assigned to is_superuser=True.')
 
+        # Creating user account for superuser.
         return self.create_user(email, user_name, first_name, password, **other_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
-
+    '''
+    Custom user model
+    '''
     email = models.EmailField(_('Email Address'), unique=True)
     user_name = models.CharField(_('Username'), max_length=150, unique=True)
     first_name = models.CharField(_('First Name'), max_length=150)
@@ -44,12 +70,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
 
+    # Mentioning the attribute to be used for login instead of username
     USERNAME_FIELD = 'email'
 
+    # Fields required to be field during user registration
     REQUIRED_FIELDS = ['user_name', 'first_name']
 
+    # Custom user account Manager class
     objects = CustomUserManager()
-
+    
     def __str__(self):
         return self.user_name
 
--- users/tests.py
+++ users/tests.py
@@ -1,23 +1,36 @@
+# Django Utility imports
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+# Testing class for User account creation testing
 class UserAccountTests(TestCase):
-
+    '''
+    This class tests the creation of simple user account as well as superuser account creation.
+    '''
     def test_new_superuser(self):
+        '''
+        Tests super user account creation.
+        '''
+        # getting the model instance for User model
         db = get_user_model()
         
+        # creating a super user account
         super_user = db.objects.create_superuser(
             'testuser@django.com', 'username', 'first_name', 'password'
         )
-
+        
+        # Testing asserts for superuser account information.
         self.assertEqual(super_user.email, 'testuser@django.com')
         self.assertEqual(super_user.user_name, 'username')
         self.assertEqual(super_user.first_name, 'first_name')
+        self.assertEqual(str(super_user), 'username')
+        
+        # Testing the statuses for superuser
         self.assertTrue(super_user.is_superuser)
         self.assertTrue(super_user.is_staff)
         self.assertTrue(super_user.is_active)
-        self.assertEqual(str(super_user), 'username')
 
+        # Testing the error handlers for wrong superuser status
         with self.assertRaises(ValueError):
             
             db.objects.create_superuser(
@@ -28,6 +41,7 @@ class UserAccountTests(TestCase):
                 is_superuser=False
             )
 
+        # Testing the error handlers for wrong staff status
         with self.assertRaises(ValueError):
             
             db.objects.create_superuser(
@@ -38,6 +52,7 @@ class UserAccountTests(TestCase):
                 is_staff=False
             )
 
+        # Testing the error handlers for empty email address
         with self.assertRaises(ValueError):
             
             db.objects.create_superuser(
@@ -49,20 +64,28 @@ class UserAccountTests(TestCase):
             )
     
     def test_new_user(self):
+        '''
+        Tests a simple user account creation
+        '''
         db = get_user_model()
 
+        # Creating the simple user account
         user = db.objects.create_user(
             'testuser1@django.com', 'username', 'first_name', 'password' 
         )
 
+        # Testing the asserts for user account info
         self.assertEqual(user.email, 'testuser1@django.com')
         self.assertEqual(user.user_name, 'username')
         self.assertEqual(user.first_name, 'first_name')
+        self.assertEqual(str(user), 'username')
+
+        # Testing the status for simple user account
         self.assertFalse(user.is_superuser)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_active)
-        self.assertEqual(str(user), 'username')
 
+        # Testing the error handlers for empty email address
         with self.assertRaises(ValueError):
             db.objects.create_user(
                 email='',
